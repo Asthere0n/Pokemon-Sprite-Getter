324e259f99cc70739dda223236a89e6789c12bba
Asthere0n <cristiansanchezsonido@gmail.com>
2024-11-17 15:08:14 +0000

    RESET functionality into GET

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import { fetchPokemonData } from "./app/modAPI.js"
 import { resetHTML } from "./app/modHTML.js"
-import {pokemonSearch, getButton, resetButton} from "./app/variables.js"
+import {pokemonSearch, getButton} from "./app/variables.js"
 
 let currentPokemon = {}
 
 //Button functionalities
 getButton.addEventListener("click",()=>{
-    currentPokemon = fetchPokemonData(pokemonSearch.value)
-    console.log(currentPokemon)
+    if (pokemonSearch.value == ''){
+        resetHTML
+    } else {
+        currentPokemon = fetchPokemonData(pokemonSearch.value)
+        console.log(currentPokemon)
+    }
 })
-
-resetButton.addEventListener('click', ()=> resetHTML())
